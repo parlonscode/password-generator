5b62de3965f26f95b0d2272ad8ed3200e8021b6d
Honore Hounwanou <mercuryseries@gmail.com>
2022-01-27 18:02:48 -0500

    Make sure that the generated password met the requested constraints

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -23,26 +23,64 @@ class PagesController extends AbstractController
         $digits = $request->query->getBoolean('digits');
         $specialCharacters = $request->query->getBoolean('special_characters');
 
-        $characters = range('a', 'z');
+        $lowercaseLettersSet = range('a', 'z');
+        $uppercaseLettersSet = range('A', 'Z');
+        $digitsSet = range(0, 9);
+        $specialCharactersSet = ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '=', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-        if ($uppercaseLetters) {
-            $characters = array_merge($characters, range('A', 'Z'));
-        }
-
-        if ($digits) {
-            $characters = array_merge($characters, range(0, 9));
-        }
-
-        if ($specialCharacters) {
-            $characters = array_merge($characters, ['!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '=', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']);
-        }
+        $characters = $lowercaseLettersSet;
 
         $password = '';
 
-        for ($i = 0; $i < $length; $i++) { 
+        // On rajoute une lettre en miniscule choisie de manière aléatoire
+        $password .= $lowercaseLettersSet[random_int(0, count($lowercaseLettersSet) - 1)];
+
+        if ($uppercaseLetters) {
+            $characters = array_merge($characters, $uppercaseLettersSet);
+
+            // On rajoute une lettre en majuscule choisie de manière aléatoire
+            $password .= $uppercaseLettersSet[random_int(0, count($uppercaseLettersSet) - 1)];
+        }
+
+        if ($digits) {
+            $characters = array_merge($characters, $digitsSet);
+
+            // On rajoute un chiffre choisi de manière aléatoire
+            $password .= $digitsSet[random_int(0, count($digitsSet) - 1)];
+        }
+
+        if ($specialCharacters) {
+            $characters = array_merge($characters, $specialCharactersSet);
+
+            // On rajoute un caractère spécial choisi de manière aléatoire
+            $password .= $specialCharactersSet[random_int(0, count($specialCharactersSet) - 1)];
+        }
+
+        $numberOfCharactersRemaining = $length - mb_strlen($password);
+
+        for ($i = 0; $i < $numberOfCharactersRemaining; $i++) { 
             $password .= $characters[random_int(0, count($characters) - 1)];
         }
 
+        $password = str_split($password);
+
+        $this->secureShuffle($password);
+
+        $password = implode('', $password);
+
         return $this->render('pages/password.html.twig', compact('password'));
+    }
+
+    private function secureShuffle(array &$arr): void
+    {
+        // Source: https://github.com/lamansky/secure-shuffle/blob/master/src/functions.php
+        $arr = array_values($arr);
+        $length = count($arr);
+        for ($i = $length - 1; $i > 0; $i--) {
+            $j = random_int(0, $i);
+            $temp = $arr[$i];
+            $arr[$i] = $arr[$j];
+            $arr[$j] = $temp;
+        }
     }
 }
