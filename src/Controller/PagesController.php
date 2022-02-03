9bf24acfd15e07cad606b1ab2cd1780a97381f6d
Honore Hounwanou <mercuryseries@gmail.com>
2022-02-03 16:49:43 -0500

    Pass values directly to generate method

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -19,14 +19,14 @@ class PagesController extends AbstractController
     #[Route('/generate-password', name: 'app_generate_password')]
     public function generatePassword(Request $request): Response
     {
-        $length = $request->query->getInt('length');
-        $uppercaseLetters = $request->query->getBoolean('uppercase_letters');
-        $digits = $request->query->getBoolean('digits');
-        $specialCharacters = $request->query->getBoolean('special_characters');
-
         $passwordGenerator = new PasswordGenerator;
 
-        $password = $passwordGenerator->generate($length, $uppercaseLetters, $digits, $specialCharacters);
+        $password = $passwordGenerator->generate(
+            $request->query->getInt('length'),
+            $request->query->getBoolean('digits'),
+            $request->query->getBoolean('uppercase_letters'),
+            $request->query->getBoolean('special_characters'),
+        );
 
         return $this->render('pages/password.html.twig', compact('password'));
     }
